584ce9bc362db559b6261be9f94e8f346f1b64fd
AnubhavGupta <anubhav.rg@berkeley.edu>
2016-03-10 14:46:30 -0800

    Preventing access to /dashboard when the user is not logged in. the magic of the decorator @login_required

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
-
+from flask.ext.login import login_required
 
 # Define the blueprint: 'dashboard'
 mod_dashboard = Blueprint('dashboard', __name__)
 
 @mod_dashboard.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('dashboard/dashboard.html',
                            title='Your Dashboard')
