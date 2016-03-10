163864719859f6a8befb0e4383419a936df4003c
AnubhavGupta <anubhav.rg@berkeley.edu>
2016-03-10 00:10:28 -0800

    fixing test case

--- tests/test_auth_models.py
+++ tests/test_auth_models.py
@@ -5,27 +5,27 @@ from models import User
 
 class TestIndex(unittest.TestCase):
 
-	def setUp(self):
+    def setUp(self):
         self.app = app.test_client()
 
     def tearDown(self):
         pass
 
     def test_is_authenticated(self):
-    	user=User('anubhav')
-    	self.assertEquals(user.is_authenticated(), 'True')
+        user=User('anubhav')
+        self.assertEquals(user.is_authenticated(), 'True')
 
     def test_is_active(self):
-    	user=User('anubhav')
-    	self.assertEquals(user.is_active(), 'True')	
+        user=User('anubhav')
+        self.assertEquals(user.is_active(), 'True')	
 
-   	def test_is_anonyous(self):
-    	user=User('anubhav')
-    	self.assertEquals(user.is_anonymous(), 'False')
+    def test_is_anonyous(self):
+        user=User('anubhav')
+        self.assertEquals(user.is_anonymous(), 'False')
 
     def test_get_id(self):
-    	user=User('anubhav')
-    	self.assertEquals(user.get_id(), 'anubhav')	
+        user=User('anubhav')
+        self.assertEquals(user.get_id(), 'anubhav')	
 
 if __name__ == '__main__':
 	unittest.main()
