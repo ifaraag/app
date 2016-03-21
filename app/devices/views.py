75aae336f98943d34e4df1aa4fadd083c2e6ae50
AnubhavGupta <anubhav.rg@berkeley.edu>
2016-03-21 12:08:06 -0700

    Dsiplay all the existing devices stored for a user when the user clicks on the devices page

--- app/devices/views.py
+++ app/devices/views.py
@@ -1,14 +1,20 @@
 from flask import Blueprint, render_template
 from app import db, login_manager, pubnub
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
+from app.auth.models import User
 
 mod_devices = Blueprint('devices', __name__)
 
 @mod_devices.route('/devices', methods=['GET'])
 @login_required
 def list_devices():
+	device_list = []
+	devices = db.devices.find({'username': current_user.get_id()})
+	for device in devices:
+		device_list.append((device['name'], device['type'], \
+				device['sensors'], device['actuators'], device['setup']))
 	return render_template('devices/device.html',
-                           title='Your Devices')
+                           title='Your Devices', my_devices=device_list)
 
 @mod_devices.route('/add_device', methods=['POST'])
 @login_required
