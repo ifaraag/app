bda7e9f70f68e69c2989f17765bfb8301c73e06f
AnubhavGupta <anubhav.rg@berkeley.edu>
2016-04-05 10:06:59 -0700

    Read sensor and actuator data from DB. Also when a devices is added, the actuator and sensor data is also added to the DB

--- app/devices/views.py
+++ app/devices/views.py
@@ -16,8 +16,7 @@ def list_devices():
 	for device in devices:
 		device_list.append((device['device_name'], device['type'], \
 				device['sensors'], device['actuators'], device['kit']))
-	return render_template('devices/devices.html' , title='Your Devices', \
-						my_devices=device_list, username=username, uuid=UUID)
+	return render_template('devices/devices.html' , title='Your Devices', my_devices=device_list, username=username, uuid=UUID)
 
 @mod_devices.route('/add_device', methods=['POST'])
 @login_required
@@ -28,7 +27,11 @@ def add_device():
 	existing_device = db.devices.find_one({'device_name' :
                                            request.form['device_name']})
 	if not existing_device:
-		new_device = {'username' : username, 'device_name' : request.form['device_name'], 'type' : 'Arduino', 'kit' : request.form['kit'], 'actuators' : [], 'sensors' : []}
+		new_device = {'username' : username, 'device_name' : request.form['device_name'], \
+		'type' : 'Arduino', 'kit' : request.form['kit'], \
+		'sensors' : ['Lux', 'Water Temperature', 'Air Temperature', 'Humidity', 'pH', 'Electrical Conductivity', 'Total Dissolved Solids', 'Practical Salinity'], \
+        'actuators': {"Light 1" : "30", "Light 2" : "31", "Water Pump" : "32", "Nutrient Pump" : "33", "pH Upper" : "34", "pH Downer" : "35"}
+        }
 		db.devices.insert_one(new_device)
 	devices = db.devices.find({'username': current_user.get_id()})
 	for device in devices:
