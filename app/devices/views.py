3e4fc2336f005d7a4a50e08eedd7bff091f5dc53
AnubhavGupta <anubhav.rg@berkeley.edu>
2016-04-06 10:31:34 -0700

    Edit functionality for each device is working

--- app/devices/views.py
+++ app/devices/views.py
@@ -10,70 +10,115 @@ mod_devices = Blueprint('devices', __name__)
 @login_required
 def list_devices():
 	device_list = []
-	UUID = uuid.uuid4()
+	UUID = str(uuid.uuid4())
 	username = current_user.get_id()
 	devices = db.devices.find({'username': current_user.get_id()})
 	for device in devices:
 		device_list.append((device['device_name'], device['type'], \
-				device['sensors'], device['actuators'], device['kit']))
+				device['sensors'], device['actuators'], device['kit'], device['device_id']))
 	return render_template('devices/devices.html' , title='Your Devices', my_devices=device_list, username=username, uuid=UUID)
 
-@mod_devices.route('/add_device', methods=['POST'])
+@mod_devices.route('/add_device/<new_device_id>', methods=['POST'])
 @login_required
-def add_device():
-	print(request.form['Lux'])
-	print(request.form['Water_Temp'])
-	print(request.form['Air_Temp'])
-	print(request.form['Humidity'])
-	print(request.form['pH'])
-	print(request.form['EC'])
-	print(request.form['TDS'])
-	print(request.form['PS'])
+def add_device(new_device_id):
 	username = current_user.get_id()
-	UUID = uuid.uuid4()
+	UUID = str(uuid.uuid4())
 	device_list = []
 	existing_device = db.devices.find_one({'device_name' :
                                            request.form['device_name']})
 	if not existing_device:
 		if request.form['kit'] == "standard":
-			new_device = {'username' : username, 'device_name' : request.form['device_name'], 'type' : 'Arduino', 'kit' : request.form['kit'], 'sensors' : ['Lux', 'Water Temperature', 'Air Temperature', 'Humidity', 'pH', 'Electrical Conductivity', 'Total Dissolved Solids', 'Practical Salinity'], 'actuators': {"Light 1" : "30", "Light 2" : "31", "Water Pump" : "32", "Nutrient Pump" : "33", "pH Upper" : "34", "pH Downer" : "35"}}
+			new_device = {'username' : username, 'device_id': device_id, 'device_name' : request.form['device_name'], 'type' : 'Arduino', 'kit' : request.form['kit'], \
+			'sensors' : ['Lux', 'Water_Temp', 'Air_Temp', 'Humidity', 'pH', 'EC', 'TDS', 'PS'], \
+			'actuators': {"light_1" : "30", "light_2" : "31", "water_pump" : "32", "nutrient_pump" : "33", "phUpper_pump" : "34", "phUpper_pump" : "35"}}
 		else:
 			sensors =[]
 			if request.form['Lux'] == 'on':
 				sensors.append("Lux")
 			if request.form['Water_Temp'] == 'on':
-				sensors.append("Water Temperature")
+				sensors.append("Water_Temp")
 			if request.form['Air_Temp'] == 'on':
-				sensors.append("Air Temperature")
+				sensors.append("Air_Temp")
 			if request.form['Humidity'] == 'on':
 				sensors.append("Humidity")
 			if request.form['pH'] == 'on':
 				sensors.append("pH")
 			if request.form['EC'] == 'on':
-				sensors.append("Electrical Conductivity")
+				sensors.append("EC")
 			if request.form['TDS'] == 'on':
-				sensors.append("Total Dissolved Solids")
+				sensors.append("TDS")
 			if request.form['PS'] == 'on':
-				sensors.append("Practical Salinity")
+				sensors.append("PS")
 			actuators = {}
 			if request.form['light_1_pin'] != "":
-				actuators['Light 1'] = request.form['light_1_pin']
+				actuators['light_1'] = request.form['light_1_pin']
 			if request.form['light_2_pin'] != "":
-				actuators['Light 2'] = request.form['light_2_pin']
+				actuators['light_2'] = request.form['light_2_pin']
 			if request.form['water_pump_pin'] != "":
-				actuators['Water Pump'] = request.form['water_pump_pin']
+				actuators['water_pump'] = request.form['water_pump_pin']
 			if request.form['nutrient_pump_pin'] != "":
-				actuators['Nutrient Pump'] = request.form['nutrient_pump_pin']
+				actuators['nutrient_pump'] = request.form['nutrient_pump_pin']
 			if request.form['phUpper_pump_pin'] != "":
-				actuators['pH Upper'] = request.form['phUpper_pump_pin']
+				actuators['phUpper_pump'] = request.form['phUpper_pump_pin']
 			if request.form['phDowner_pump_pin'] != "":
-				actuators['pH Downer'] = request.form['phDowner_pump_pin']
-			new_device = {'username' : username, 'device_name' : request.form['device_name'], 'type' : request.form['device_type'], 'kit' : request.form['kit'], 'sensors' : sensors, 'actuators': actuators}
+				actuators['phDowner_pump'] = request.form['phDowner_pump_pin']
+			new_device = {'username' : username, 'device_id': device_id, 'device_name' : request.form['device_name'], 'type' : request.form['device_type'], 'kit' : request.form['kit'], 'sensors' : sensors, 'actuators': actuators}
 		db.devices.insert_one(new_device)
 	devices = db.devices.find({'username': current_user.get_id()})
 	for device in devices:
 		device_list.append((device['device_name'], device['type'], \
-	device['sensors'], device['actuators'], device['kit']))
+	device['sensors'], device['actuators'], device['kit'], device['device_id']))
+	return render_template('devices/devices.html' , title='Your Devices', \
+		my_devices=device_list, username=username, uuid=UUID)
+
+@mod_devices.route('/edit_device/<device_id>', methods=['POST'])
+@login_required
+def edit_device(device_id):
+	username = current_user.get_id()
+	UUID = str(uuid.uuid4())
+	device_list = []
+	sensors =[]
+	if request.form['Lux'] == 'on':
+		sensors.append("Lux")
+	if request.form['Water_Temp'] == 'on':
+		sensors.append("Water_Temp")
+	if request.form['Air_Temp'] == 'on':
+		sensors.append("Air_Temp")
+	if request.form['Humidity'] == 'on':
+		sensors.append("Humidity")
+	if request.form['pH'] == 'on':
+		sensors.append("pH")
+	if request.form['EC'] == 'on':
+		sensors.append("EC")
+	if request.form['TDS'] == 'on':
+		sensors.append("TDS")
+	if request.form['PS'] == 'on':
+		sensors.append("PS")
+	print(sensors)
+	actuators = {}
+	if request.form['light_1_pin'] != "":
+		actuators['light_1'] = request.form['light_1_pin']
+	if request.form['light_2_pin'] != "":
+		actuators['light_2'] = request.form['light_2_pin']
+	if request.form['water_pump_pin'] != "":
+		actuators['water_pump'] = request.form['water_pump_pin']
+	if request.form['nutrient_pump_pin'] != "":
+		actuators['nutrient_pump'] = request.form['nutrient_pump_pin']
+	if request.form['phUpper_pump_pin'] != "":
+		actuators['phUpper_pump'] = request.form['phUpper_pump_pin']
+	if request.form['phDowner_pump_pin'] != "":
+		actuators['phDowner_pump'] = request.form['phDowner_pump_pin']
+	result = db.devices.update_one(
+      { "device_id" : device_id },
+      {
+      '$set': {'sensors' : sensors, 'actuators' : actuators}
+      },
+      upsert=True
+      )
+	devices = db.devices.find({'username': current_user.get_id()})
+	for device in devices:
+		device_list.append((device['device_name'], device['type'], \
+	device['sensors'], device['actuators'], device['kit'], device['device_id']))
 	return render_template('devices/devices.html' , title='Your Devices', \
 		my_devices=device_list, username=username, uuid=UUID)
 
