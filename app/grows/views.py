25f6b08f5948c4e23b62855041248691166942b1
AnubhavGupta <anubhav.rg@berkeley.edu>
2016-04-12 23:15:37 -0700

    Editing grows functionality added

--- app/grows/views.py
+++ app/grows/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app import db, login_manager, pubnub
 from flask.ext.login import login_required, current_user
 
@@ -16,7 +16,7 @@ def list_grow(current_grow):
 	grows = db.grows.find({'grow_name' : current_grow})
 	for grow in grows:
 		assoc_device_name = grow['device_name']
-		grows_list.append((current_grow, grow['device_name']))
+		grows_list.append((current_grow, grow['device_name'], grow['sensors'], grow['actuators']))
 	devices = db.devices.find({'device_name': assoc_device_name })
 	for device in devices:
 		device_list.append((device['device_name'], device['type'], \
@@ -28,7 +28,7 @@ def list_grow(current_grow):
 				device['sensors'], device['actuators'], device['kit'], device['device_id']))
 	grows = db.grows.find({'username' : current_user.get_id()})
 	for grow in grows:
-		user_grows.append((grow['grow_name'], grow['device_name']))
+		user_grows.append((grow['grow_name'], grow['device_name'], grow['sensors'], grow['actuators']))
 	
 	return render_template('grows/grows.html',
                            title='Your Grows', username=username, current_grow=current_grow, current_device=assoc_device_name, \
@@ -43,7 +43,7 @@ def link(current_grow, link_device):
 	result = db.grows.update_one(
       { "grow_name" : current_grow},
       {
-      '$set': {'device_name' : link_device, 'device_id' : device_id}
+      '$set': {'device_name' : link_device, 'device_id' : device_id, 'sensors':[], 'actuators' : {}}
       },
       upsert=True
       )
@@ -56,7 +56,7 @@ def link(current_grow, link_device):
 	grows = db.grows.find({'grow_name' : current_grow})
 	for grow in grows:
 		assoc_device_name = grow['device_name']
-		grows_list.append((current_grow, grow['device_name']))
+		grows_list.append((current_grow, grow['device_name'], grow['sensors'], grow['actuators']))
 	devices = db.devices.find({'device_name': assoc_device_name })
 	for device in devices:
 		device_list.append((device['device_name'], device['type'], \
@@ -68,9 +68,83 @@ def link(current_grow, link_device):
 				device['sensors'], device['actuators'], device['kit'], device['device_id']))
 	grows = db.grows.find({'username' : current_user.get_id()})
 	for grow in grows:
-		user_grows.append((grow['grow_name'], grow['device_name']))
+		user_grows.append((grow['grow_name'], grow['device_name'], grow['sensors'], grow['actuators']))
 	
 	return render_template('grows/grows.html',
                            title='Your Grows', username=username, current_grow=current_grow, current_device=assoc_device_name, \
                            device=device_list, grow=grows_list, my_devices=user_devices, my_grows=user_grows)
+
+
+@mod_grows.route('/edit_grows/<current_grow>', methods=['POST'])
+@login_required
+def edit_grow(current_grow):
+	sensors =[]
+	if request.form['Lux'] == 'on':
+		sensors.append("Lux")
+	if request.form['Water_Temp'] == 'on':
+		sensors.append("Water_Temp")
+	if request.form['Air_Temp'] == 'on':
+		sensors.append("Air_Temp")
+	if request.form['Humidity'] == 'on':
+		sensors.append("Humidity")
+	if request.form['pH'] == 'on':
+		sensors.append("pH")
+	if request.form['EC'] == 'on':
+		sensors.append("EC")
+	if request.form['TDS'] == 'on':
+		sensors.append("TDS")
+	if request.form['PS'] == 'on':
+		sensors.append("PS")
+	grows = db.grows.find({'grow_name':current_grow})
+	for grow in grows:
+		device_id = grow['device_id']
+	devices = db.devices.find({'device_id':device_id})
+	for device in devices:
+		act_pins = device['actuators']
+	actuators ={}
+	for key in act_pins.keys():
+		if request.form[key] == "on":
+			actuators[key] = act_pins[key]
+	grows = db.grows.find({'grow_name':current_grow})
+	for grow in grows:
+		device_id = device['device_id']
+	result = db.grows.update_one(
+      { "grow_name" : current_grow},
+      {
+      '$set': {'sensors':sensors, 'actuators' : actuators}
+      },
+      upsert=True
+      )
+	user_devices = []
+	user_grows = []
+	device_list = []
+	grows_list = []
+	assoc_device_name = ''
+	username = current_user.get_id()
+	grows = db.grows.find({'grow_name' : current_grow})
+	for grow in grows:
+		assoc_device_name = grow['device_name']
+		grows_list.append((current_grow, grow['device_name'], grow['sensors'], grow['actuators']))
+	devices = db.devices.find({'device_name': assoc_device_name })
+	for device in devices:
+		device_list.append((device['device_name'], device['type'], \
+				device['sensors'], device['actuators'], device['kit'], device['device_id']))
+
+	devices = db.devices.find({'username': current_user.get_id()})
+	for device in devices:
+		user_devices.append((device['device_name'], device['type'], \
+				device['sensors'], device['actuators'], device['kit'], device['device_id']))
+	grows = db.grows.find({'username' : current_user.get_id()})
+	for grow in grows:
+		user_grows.append((grow['grow_name'], grow['device_name'], grow['sensors'], grow['actuators']))
+	
+	return render_template('grows/grows.html',
+                           title='Your Grows', username=username, current_grow=current_grow, current_device=assoc_device_name, \
+                           device=device_list, grow=grows_list, my_devices=user_devices, my_grows=user_grows)
+
+
+
+
+
+
 
