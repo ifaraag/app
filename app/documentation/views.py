8e35b2ccffc894aceeb45f63f93c56da4e15e241
brynamo <brynamo@berkeley.edu>
2016-05-06 03:54:37 -0700

    adding documentation

--- app/documentation/views.py
+++ app/documentation/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, Response
 from flask.ext.login import login_required, current_user
+from app import db
+import json
 
 # Define the blueprint: 'documentation'
 mod_documentation = Blueprint('documentation', __name__)
@@ -7,4 +9,10 @@ mod_documentation = Blueprint('documentation', __name__)
 @mod_documentation.route('/documentation')
 @login_required
 def documentation():
-	pass
+	username = current_user.get_id()
+	grows_list = []
+	grows = db.grows.find({'username' : current_user.get_id()})
+	for grow in grows:
+		grows_list.append((grow['grow_name'], grow['device_name'], grow['sensors'], grow['actuators']))
+	
+	return render_template('documentation/documentation.html', username=username, my_grows=grows_list)
