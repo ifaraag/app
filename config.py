55ee277fc77ba065366e41548a4db3deeafe31bf
Ibrahim Emad Farrag <ibrahim.emad.farrag@gmail.com>
2020-06-21 19:26:36 +0200

    debug on

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
+DEBUG = True
 
 SRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
@@ -14,4 +14,4 @@ PUBNUB_PUBLISH_KEY = 'pub-c-d820df08-5466-4cfc-a868-704d857db647'
 PUBNUB_SUBSCRIBE_KEY = 'sub-c-942b13b6-b313-11ea-875a-ceb74ea8e96a'
 PUBNUB_SECRET_KEY = 'sec-c-OTAyOTY0NWUtNzRhZS00Nzg4LWJmNmItOWMyYjQwYWFkZWYy'
 PUBNUB_AUTH_KEY = 'auth-me-not'
-PUBNUB_CHANNEL_GRP = "hydrobase"
+PUBNUB_CHANNEL_GRP = "hydrosmart"
