4707dfdf7a889c61123919ff54de1a8404757027
Ibrahim Emad Farrag <ibrahim.emad.farrag@gmail.com>
2020-06-21 18:58:02 +0200

    [mongo db links and keys]

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DEBUG = False
 SRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-MONGO_URI = 'mongodb+srv://admin:admin@cluster0-4w9h4.gcp.mongodb.net/hydrosmart?retryWrites=true&w=majority'
+MONGO_URI = "mongodb+srv://admin:admin@cluster0-4w9h4.gcp.mongodb.net/hydrosmart?retryWrites=true&w=majority"
 DB_NAME = 'hydrosmart'
 
 
@@ -14,4 +14,4 @@ PUBNUB_PUBLISH_KEY = 'pub-c-d820df08-5466-4cfc-a868-704d857db647'
 PUBNUB_SUBSCRIBE_KEY = 'sub-c-942b13b6-b313-11ea-875a-ceb74ea8e96a'
 PUBNUB_SECRET_KEY = 'sec-c-OTAyOTY0NWUtNzRhZS00Nzg4LWJmNmItOWMyYjQwYWFkZWYy'
 PUBNUB_AUTH_KEY = 'auth-me-not'
-PUBNUB_CHANNEL_GRP = "hydrosmart"
+PUBNUB_CHANNEL_GRP = "hydrobase"
